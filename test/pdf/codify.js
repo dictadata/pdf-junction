baad44d2a672a74c078ac918f020a316ec393135
drewlab <drew@dictadata.org>
2024-04-22 09:45:42 -0500

    refactor _codify tests, use terminal.output

--- test/pdf/codify.js
+++ test/pdf/codify.js
@@ -18,7 +18,9 @@ async function tests() {
         headers: [ "Greating" ]
       }
     },
-    output: "./test/data/output/pdf/codify_0.json"
+    "terminal": {
+      output: "./test/data/output/pdf/codify_0.json"
+    }
   })) return 1;
 
   logger.verbose("=== codify ClassCodes.pdf");
@@ -27,31 +29,54 @@ async function tests() {
       smt: "pdf|./test/data/input/pdf/|ClassCodes.pdf|*",
       options: {}
     },
-    output: "./test/data/output/pdf/codify_1.json"
+    "terminal": {
+      output: "./test/data/output/pdf/codify_1.json"
+    }
   })) return 1;
 
   logger.verbose("=== codify Nat_State_Topic_File_formats.pdf");
   if (await codify({
     origin: {
-      smt: "pdf|./test/data/input/pdf/Nat_State_Topic_File_formats.pdf||*",
+      smt: "pdf|./test/data/input/pdf/|Nat_State_Topic_File_formats.pdf|*",
       options: {
         heading: "Government Units File Format",
         cells: 3
       }
     },
-    output: "./test/data/output/pdf/codify_2.json"
+    "terminal": {
+      output: "./test/data/output/pdf/codify_2.json"
+    }
   })) return 1;
 
   logger.verbose("=== codify CongNov22.pdf");
   if (await codify({
     origin: {
-      smt: "pdf|./test/data/input/pdf/CongNov22.pdf||*",
+      smt: "pdf|./test/data/input/pdf/|CongNov22.pdf|*",
       options: {
         heading: "US Representative District 1",
         cells: 10
       }
     },
-    output: "./test/data/output/pdf/codify_3.json"
+    "terminal": {
+      output: "./test/data/output/pdf/codify_3.json"
+    }
+  })) return 1;
+
+  logger.verbose("=== codify voter_registration");
+  if (await codify({
+    origin: {
+      smt: "pdf|file:/var/dictadata/US/AZ/azsos.gov/election/VoterReg/2024/|state_voter_registration_jan2024.pdf|*",
+      "options": {
+        "pages": [ 1 ],
+        "pageHeader": 64,
+        "cells": 9,
+        "RepeatCell.column": 0,
+        "missingValue": "*"
+      }
+    },
+    "terminal": {
+      output: "./test/data/output/pdf/codify_4.json"
+    }
   })) return 1;
 }
 
